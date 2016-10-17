62815283bf09a976e906c88d289deb15359ff573
LGM1213 <lgm1213@gmail.com>
2016-10-17 19:49:32 -0400

    Url Parser project upload

---
+++ spec/testing.rb
@@ -0,0 +1,24 @@
+
+
+
+@new_url = "http://www.google.com:60/search?q=cat&q=overwrite#img=FunnyCat"
+#.split("?").last.split("#").first.split("&").first.split("=")
+
+
+def query_string
+	@query_string = @new_url.split("?").last.split("#").first
+    @split1 = @query_string.split("&")
+    @split1.map! do |value|
+       value.split("=")
+   end
+@split1.to_h
+end
+
+p query_string
+# Bhash = Hash[keys, value]
+
+
+# def query_string
+# 		@query_string = @new_url.split("?").last.split("#").first.split("&").first.split("=")
+# 		@query_string = Hash[*query_string_zero.flatten]
+# 	end	
---
+++ url_parser.rb
@@ -0,0 +1,55 @@
+class UrlParser
+
+	def initialize(new_url)
+		@new_url = new_url
+	end
+
+	def scheme
+		@scheme = @new_url.split(":").first
+	end
+
+	def domain
+		@domain = @new_url.split("//").last.split(":").first
+	end
+
+	def port
+		@port = @new_url.split(":").last.split("/").first
+		if @port == "" && scheme == "http"
+			return "80"
+		elsif @port == "" && scheme == "https"
+			return "443"
+		else
+			@port
+		end
+	end
+
+	def path
+		@path = @new_url.split("/").last.split("?").first
+		if @path == "" 
+			return nil
+		else
+			@path
+		end
+	end
+
+	def query_string
+		@query_string = @new_url.split("?").last.split("#").first
+    	@query_string_zero = @query_string.split("&")
+    	@query_string_zero.map! do |value|
+       		value.split("=")
+   		end
+		@query_string_zero.to_h
+	end
+
+	def fragment_id
+		@fragment_id = @new_url.split("#").last
+	end
+
+
+end
+
+
+
+
+
+#~/Desktop/WynCode/url_parser/url_parser.rb
