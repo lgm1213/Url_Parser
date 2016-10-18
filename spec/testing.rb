9a2c5fcc0c8a0f4ad5425d8176a177b696e2c2d5
LGM1213 <lgm1213@gmail.com>
2016-10-18 09:01:36 -0400

    Git Portfolio 2

--- spec/testing.rb
+++ spec/testing.rb
@@ -5,20 +5,20 @@
 #.split("?").last.split("#").first.split("&").first.split("=")
 
 
+# def query_string
+# 	@query_string = @new_url.split("?").last.split("#").first
+#     @split1 = @query_string.split("&")
+#     @split1.map! do |value|
+#        value.split("=")
+#    end
+# @split1.to_h
+# end
+
+
 def query_string
-	@query_string = @new_url.split("?").last.split("#").first
-    @split1 = @query_string.split("&")
-    @split1.map! do |value|
-       value.split("=")
-   end
-@split1.to_h
-end
+		@query_string = @new_url.split("?").last.split("#").first.split("&").first.split("=")
+		@query_string = Hash[*query_string_zero.flatten]
+	end	
 
 p query_string
 # Bhash = Hash[keys, value]
-
-
-# def query_string
-# 		@query_string = @new_url.split("?").last.split("#").first.split("&").first.split("=")
-# 		@query_string = Hash[*query_string_zero.flatten]
-# 	end	
